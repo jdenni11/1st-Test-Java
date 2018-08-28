e3b94180cbd3ffd5d03c0af763c66de91a5b0376
Joshua Dennis <42750706+jdenni11@users.noreply.github.com>
2018-08-28 13:46:02 -0400

    Test of first commit

---
+++ Test.java
@@ -0,0 +1,18 @@
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		int [] numbers = new int[5];
+
+		for(int i = 0; i < numbers.length; i++) {
+			numbers[i] = i *10;
+		}
+
+	for(int i = 0; i < numbers.length; i++) {
+		System.out.println(numbers[i]);
+		}
+
+	}
+
+}
